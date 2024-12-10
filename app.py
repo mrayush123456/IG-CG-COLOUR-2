7168c60564cb2dd717154077fc0b8b058417c964
mrayush123456 <yk7129388@gmail.com>
2024-12-10 14:22:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,4 +156,5 @@ def home():
     return render_template_string(HTML_TEMPLATE)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+                        
